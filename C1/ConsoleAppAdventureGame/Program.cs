cafd6708a3b8938f5854389fcddce430a8d51d84
luiscmt22 <159567497+luiscmt22@users.noreply.github.com>
2026-05-19 17:43:18 +0100

    Add initial implementation of StoryNode class and sample node data

--- C1/ConsoleAppAdventureGame/Program.cs
+++ C1/ConsoleAppAdventureGame/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+
+
+StoryNode node = new()
+{
+
+    Text = 
+    [
+        "You wake up in a dark room. You can't remember how you got there.",
+        "You see a door to your left and a window to your right."
+    ],
+    
+    index = 0
+};
+
+
+public class StoryNode()
+{
+    public string Id => 4.ToString();
+    public int index { get; init; }
+    public required string[] Text { get; init; }
+    //public Choice[] Choices { get; init; } = [];
+}
